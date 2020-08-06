2b14ae9036a03bb91cfc3d3d8b017f1a491f4c21
yehongzhi <yehongzhi@do1.com.cn>
2020-08-07 01:56:58 +0800

    整合阿里canal框架

---
+++ canal/src/main/java/io/github/yehongzhi/canal/CanalApplication.java
@@ -0,0 +1,13 @@
+package io.github.yehongzhi.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class, args);
+    }
+
+}
--- core/src/main/resources/sql/tb_commodity_info.sql
+++ core/src/main/resources/sql/tb_commodity_info.sql
@@ -6,3 +6,7 @@ CREATE TABLE `tb_commodity_info` (
   `description` varchar(2048) DEFAULT '' COMMENT '商品描述',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商品信息表';
+
+INSERT INTO tb_commodity_info VALUES('3e71a81fd80711eaaed600163e046cc3','叉烧包','3.99',3,'又大又香的叉烧包，老人小孩都喜欢');
+
+DROP TABLE `tb_commodity_info`;
